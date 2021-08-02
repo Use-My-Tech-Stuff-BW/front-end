fb6305be242d308054872e7447c42600252a3fce
thecuriousones <tamaraleonard46@gmail.com>
2021-08-01 20:20:39 -0400

    vercel test

---
+++ my-app/src/components/RenterDashboard/RenterDashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const RenterDashboard = () => {
+
+    return (
+        <div className='bg-gray-300'>
+            
+            <Link to='/' className='logout'>Log Out</Link>
+        </div>
+    )
+}
+
+export default RenterDashboard;
